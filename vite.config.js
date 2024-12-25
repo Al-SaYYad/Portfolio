de1f76254925a8a09d83df3d035ec30b76e2f67f
7oda_Al-Sayyad <142691985+Al-SaYYad@users.noreply.github.com>
2024-12-25 23:06:28 +0200

    Done

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // يضمن أن المسارات النسبية تعمل بشكل صحيح
+  build: {
+    outDir: 'dist'
+  }
 })
