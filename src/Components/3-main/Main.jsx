2daddeedc1a458787cb77f97565a8a2753c97883
7oda_Al-Sayyad <142691985+Al-SaYYad@users.noreply.github.com>
2024-12-26 21:48:10 +0200

    Notify

--- src/Components/3-main/Main.jsx
+++ src/Components/3-main/Main.jsx
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./main.css";
 import { AnimatePresence, motion } from "motion/react";
 import { myProjects } from "./data";
+import { ToastContainer, toast, Bounce } from "react-toastify";
 
 const Main = () => {
   const [arr, setArr] = useState(myProjects);
   const [currentActive, setCurrentActive] = useState("all");
+  const notify = () => toast.info("Oops! site under maintenance");
+  const [theme, setTheme] = useState(localStorage.getItem("currentMode"));
+
+  useEffect(() => {
+    setTheme(localStorage.getItem("currentMode"));
+  });
 
   const handleFilter = (buttonCat) => {
     setCurrentActive(buttonCat);
     const newArr = myProjects.filter((item) => {
-      const cat = item.category.find((i) => {
-        return i === buttonCat;
-      });
-      return cat === buttonCat;
+      const cat = item.category.find((i) => i === buttonCat);
+      return buttonCat === "all" || cat === buttonCat;
     });
     setArr(newArr);
+  };
+  const handleLinkClick = (e, link) => {
+    if (!link || link.trim() === "") {
+      e.preventDefault();
+      notify();
+    }
   };
 
   return (
@@ -72,6 +83,14 @@ const Main = () => {
         </button> */}
       </section>
       <section className="right-section flex">
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          draggable
+          pauseOnHover
+          theme={theme}
+          transition={Bounce}
+        />
         <AnimatePresence>
           {arr.map((item, index) => {
             return (
@@ -83,7 +102,10 @@ const Main = () => {
                 key={index}
                 className="card"
               >
-                <a href={item.linkProject}>
+                <a
+                  href={item.linkProject}
+                  onClick={(e) => handleLinkClick(e, item.linkProject)}
+                >
                   <img width={266} src={item.imgPath} alt="Card Image" />
                 </a>
                 <div style={{ width: "266px" }} className="box">
@@ -92,9 +114,6 @@ const Main = () => {
                   <div className="flex icons">
                     <div className="flex">
                       <a target="_blank" href={item.linkGit}>
-                        <div className="icon-link"></div>
-                      </a>
-                      <a target="_blank" href={item.linkGit}>
                         <div className="icon-github"></div>
                       </a>
                     </div>
@@ -102,8 +121,11 @@ const Main = () => {
                       className="link flex"
                       target="_blank"
                       href={item.linkProject}
+                      onClick={(e) => {
+                        handleLinkClick(e, item.linkProject);
+                      }}
                     >
-                      more <span className="icon-long-arrow-right"></span>
+                      View <span className="icon-long-arrow-right"></span>
                     </a>
                   </div>
                 </div>
